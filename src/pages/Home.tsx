fdae0ea6f9f48d9287e1356582b250f71093433a
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-10 16:01:50 -0600

    :sparkles: Add page layout to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import PageLayout from '../components/Layout/PageLayout';
 
 export default function Home() {
-  return <div className='p-6 bg-gray-200 text-2xl'>Home</div>;
+  return (
+    <PageLayout>
+      <div className='p-6 bg-gray-200 text-2xl'>Home</div>
+    </PageLayout>
+  );
 }
