c46d094c3c7da1c11cd65cea387b13f83d14e2c4
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-10 15:20:47 -0600

    :tada: Add initial template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-2xl bg-gray-200'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
