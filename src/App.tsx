ea86de9b213523d0f4d769779b9a7cabbe768446
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-10 15:34:19 -0600

    :sparkles: Add homepage to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
