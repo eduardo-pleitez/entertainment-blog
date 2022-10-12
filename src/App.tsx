398ad84b029a3de49f1db9353ca102c897c673f4
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-12 15:06:15 -0600

    :sparkles: Add MovieDetails page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/movie/:movieId' element={<MovieDetails />} />
     </Routes>
   );
 }
---
+++ src/pages/MovieDetails.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import { BASE_URL } from '../assets/globalVariables';
+import LoadingMessage from '../components/Layout/LoadingMessage';
+import ErrorMessage from '../components/Layout/ErrorMessage';
+
+export default function MovieDetails() {
+  const params = useParams();
+  const url = `${BASE_URL}movie/${params.movieId}?language=en-US`;
+  const { data, loading, error } = useFetch(url);
+
+  return (
+    <div className='sm:py-4 py-2'>
+      {loading ? <LoadingMessage /> : null}
+      {error ? <ErrorMessage>{error}</ErrorMessage> : null}
+    </div>
+  );
+}
