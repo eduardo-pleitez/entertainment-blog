ebf188f552fc6ea3154b6c7da49591e68b476466
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-12 13:35:06 -0600

    :sparkles: Add TvShowsList

---
+++ src/components/TvShows/TvShowsList.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import useFetch from '../../hooks/useFetch';
+import { BASE_URL } from '../../assets/globalVariables';
+import SectionTitle from '../Layout/SectionTitle';
+import LoadingMessage from '../Layout/LoadingMessage';
+import ErrorMessage from '../Layout/ErrorMessage';
+
+export default function TvShowsList() {
+  const [url, setUrl] = useState(`${BASE_URL}tv/popular?language=en-US`);
+  const { data, loading, error, totalPages } = useFetch(url);
+  return (
+    <div className='sm:py-4 py-2'>
+      <SectionTitle>TV Shows</SectionTitle>
+      <>
+        {loading ? (
+          <LoadingMessage />
+        ) : (
+          null
+        )}
+        {error ? <ErrorMessage>{error}</ErrorMessage> : null}
+      </>
+    </div>
+  );
+}
