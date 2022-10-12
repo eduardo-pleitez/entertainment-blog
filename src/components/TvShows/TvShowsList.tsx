c903f0591da32ab6b8f9f0478dddb9b4f82635f8
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-12 13:49:13 -0600

    :art: Format code

--- src/components/TvShows/TvShowsList.tsx
+++ src/components/TvShows/TvShowsList.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import useFetch from '../../hooks/useFetch';
 import { BASE_URL } from '../../assets/globalVariables';
 import SectionTitle from '../Layout/SectionTitle';
@@ -10,18 +10,16 @@ import Pagination from '../Layout/Pagination';
 export default function TvShowsList() {
   const [url, setUrl] = useState(`${BASE_URL}tv/popular?language=en-US`);
   const { data, loading, error, totalPages } = useFetch(url);
-  function handlePagination(selectedItem: { selected: number; }) {
-    setUrl(`${BASE_URL}tv/popular?language=en-US&page=${selectedItem.selected + 1}`);
+  function handlePagination(selectedItem: { selected: number }) {
+    setUrl(
+      `${BASE_URL}tv/popular?language=en-US&page=${selectedItem.selected + 1}`
+    );
   }
   return (
     <div className='sm:py-4 py-2'>
       <SectionTitle>TV Shows</SectionTitle>
       <>
-        {loading ? (
-          <LoadingMessage />
-        ) : (
-          <TvShowElement data={data} />
-        )}
+        {loading ? <LoadingMessage /> : <TvShowElement data={data} />}
         {error ? <ErrorMessage>{error}</ErrorMessage> : null}
       </>
       <Pagination totalPages={totalPages} handlePagination={handlePagination} />
