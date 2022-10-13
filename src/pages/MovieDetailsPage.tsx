008cf0b95c5c8f05b2779b2b4352ef15279d9cf7
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-13 09:06:12 -0600

    :sparkles: Add page layout to movie details page

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import PageLayout from '../components/Layout/PageLayout';
 import MovieDetails from '../components/Movies/MovieDetails';
 
 export default function MovieDetailsPage() {
   return (
-    <div className='lg:w-9/12 md:w-11/12 sm:w-full bg-white md:p-6 p-2 mx-auto'>
-      <MovieDetails />
-    </div>
+    <PageLayout>
+      <div className='lg:w-9/12 md:w-11/12 sm:w-full bg-white md:p-6 p-2 mx-auto'>
+        <MovieDetails />
+      </div>
+    </PageLayout>
   );
 }
