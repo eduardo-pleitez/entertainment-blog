9351738e6fe691a214fb6bf04186b768923ec298
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-14 14:41:46 -0600

    :sparkles: Add tv show credits

---
+++ src/components/TvShows/TvShowCredits.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { BASE_URL } from '../../assets/globalVariables';
+import LoadingMessage from '../Layout/LoadingMessage';
+import ErrorMessage from '../Layout/ErrorMessage';
+import useFetchCredits from '../../hooks/useFetchCredits';
+import CreditsSection from '../Layout/CreditsSection';
+
+export default function TvShowCredits() {
+  const params = useParams();
+  const url = `${BASE_URL}tv/${params.tvShowId}/credits?language=en-US`;
+  const { data, loading, error } = useFetchCredits(url);
+  return (
+    <div>
+      {loading ? <LoadingMessage /> : null}
+      {error ? <ErrorMessage>{error}</ErrorMessage> : <CreditsSection data={data} />}
+    </div>
+  );
+}
--- src/pages/TvShowDetailsPage.tsx
+++ src/pages/TvShowDetailsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageLayout from '../components/Layout/PageLayout';
+import TvShowCredits from '../components/TvShows/TvShowCredits';
 import TvShowDetails from '../components/TvShows/TvShowDetails';
 
 export default function TvShowDetailsPage() {
@@ -7,6 +8,7 @@ export default function TvShowDetailsPage() {
     <PageLayout>
       <div className='lg:w-9/12 md:w-11/12 sm:w-full bg-white md:p-6 p-2 mx-auto'>
         <TvShowDetails />
+        <TvShowCredits />
       </div>
     </PageLayout>
   );
