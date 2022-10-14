fad161a327e24ef8e5b0e4800b83168f8985b745
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-14 14:48:23 -0600

    :sparkles: Add tv show reviews

--- src/pages/TvShowDetailsPage.tsx
+++ src/pages/TvShowDetailsPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageLayout from '../components/Layout/PageLayout';
 import TvShowCredits from '../components/TvShows/TvShowCredits';
 import TvShowDetails from '../components/TvShows/TvShowDetails';
+import TvShowReviews from '../components/TvShows/TvShowReviews';
 
 export default function TvShowDetailsPage() {
   return (
@@ -9,6 +10,7 @@ export default function TvShowDetailsPage() {
       <div className='lg:w-9/12 md:w-11/12 sm:w-full bg-white md:p-6 p-2 mx-auto'>
         <TvShowDetails />
         <TvShowCredits />
+        <TvShowReviews />
       </div>
     </PageLayout>
   );
