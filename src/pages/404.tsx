c612da161bf17085ae51930b41fc082648f740c7
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-17 15:32:26 -0600

    :lipstick: Update 404 page styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
   return (
-    <main className="absolute inset-0 z-0 flex flex-col justify-center items-center bg-gray-900">
+    <main className="absolute inset-0 flex flex-col justify-center items-center bg-gray-900">
       <h2 className='text-white text-2xl mb-3'>Page not found</h2>
       <h1 className="sm:text-9xl text-7xl font-extrabold text-white tracking-widest">404</h1>
       <button className="mt-5 p-5 text-white border border-white">
