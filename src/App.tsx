05ec15eda40ab284424070ff377c3fb0c925dad3
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-17 09:25:38 -0600

    :sparkles: Add season details page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MovieDetailsPage from './pages/MovieDetailsPage';
 import TvShowDetailsPage from './pages/TvShowDetailsPage';
+import SeasonDetailsPage from './pages/SeasonDetailsPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/movie/:movieId' element={<MovieDetailsPage />} />
       <Route path='/tvshow/:tvShowId' element={<TvShowDetailsPage />} />
+      <Route path='/tvshow/:tvShowId/season/:seasonId' element={<SeasonDetailsPage />} />
     </Routes>
   );
 }
---
+++ src/pages/SeasonDetailsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from '../components/Layout/PageLayout';
+
+export default function SeasonDetailsPage() {
+  return (
+    <PageLayout>
+      <div className='lg:w-9/12 md:w-11/12 sm:w-full bg-white md:p-6 p-2 mx-auto'>
+      </div>
+    </PageLayout>
+  );
+}
