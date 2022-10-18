e29701dadda6dce3b756d4b2248a8e53b1e01379
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-18 08:37:13 -0600

    :sparkles: Add search page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import TvShowDetailsPage from './pages/TvShowDetailsPage';
 import SeasonDetailsPage from './pages/SeasonDetailsPage';
 import ErrorPage from './pages/404';
 import PersonDetailsPage from './pages/PersonDetailsPage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Route path='/tvshow/:tvShowId' element={<TvShowDetailsPage />} />
       <Route path='/tvshow/:tvShowId/season/:seasonId' element={<SeasonDetailsPage />} />
       <Route path='/person/:personId' element={<PersonDetailsPage />} />
+      <Route path='/search' element={<SearchPage />} />
     </Routes>
   );
 }
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from '../components/Layout/PageLayout';
+
+export default function SearchPage() {
+  return (
+    <PageLayout>
+      <div>
+      </div>
+    </PageLayout>
+  );
+}
