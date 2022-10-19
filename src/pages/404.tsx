cb6d1657a1b34b342576dd43fbb2933d463a7959
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-18 19:06:29 -0600

    :sparkles: Update error page button

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,9 +6,11 @@ export default function ErrorPage() {
     <main className="absolute inset-0 flex flex-col justify-center items-center bg-gray-900">
       <h2 className='text-white text-2xl mb-3'>Page not found</h2>
       <h1 className="sm:text-9xl text-7xl font-extrabold text-white tracking-widest">404</h1>
-      <button className="mt-5 p-5 text-white border border-white">
-        <Link to="/">Go Home</Link>
-      </button>
+      <Link to="/">
+        <button className="mt-5 p-5 text-white border border-white">
+        Go Home
+        </button>
+      </Link>
     </main>
   );
 }
