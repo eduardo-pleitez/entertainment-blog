dd3298ea62a47d106578e661b3338150d0a3913b
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-19 18:38:14 -0600

    :recycle: Refactor search code

--- src/components/TvShows/TvShowsSearch.tsx
+++ src/components/TvShows/TvShowsSearch.tsx
@@ -5,6 +5,8 @@ import { BASE_URL } from '../../assets/globalVariables';
 
 export default function TvShowsSearch({handleFiltersChange}:PropsSearch) {
   const [genresData, setGenresData] = useState([]);
+  const yearsFilter = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 
+    2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,29 +41,11 @@ export default function TvShowsSearch({handleFiltersChange}:PropsSearch) {
       </select>
       <select name='searchTvShowsByYear' id='searchTvShowsByYear' className='p-2 bg-white border border-black sm:w-auto w-full' onChange={handleFiltersChange}>
         <option value=''>Filter By Year:</option>
-        <option value='2000'>2000</option>
-        <option value='2001'>2001</option>
-        <option value='2002'>2002</option>
-        <option value='2003'>2003</option>
-        <option value='2004'>2004</option>
-        <option value='2005'>2005</option>
-        <option value='2006'>2006</option>
-        <option value='2007'>2007</option>
-        <option value='2008'>2008</option>
-        <option value='2009'>2009</option>
-        <option value='2010'>2010</option>
-        <option value='2011'>2011</option>
-        <option value='2012'>2012</option>
-        <option value='2013'>2013</option>
-        <option value='2014'>2014</option>
-        <option value='2015'>2015</option>
-        <option value='2016'>2016</option>
-        <option value='2017'>2017</option>
-        <option value='2018'>2018</option>
-        <option value='2019'>2019</option>
-        <option value='2020'>2020</option>
-        <option value='2021'>2021</option>
-        <option value='2022'>2022</option>
+        {yearsFilter.map((year, index) => {
+          return (
+            <option value={year} key={index}>{year}</option>
+          );
+        })}
       </select>
     </div>
   );
