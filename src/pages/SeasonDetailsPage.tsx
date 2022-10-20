738e0ea3d01328264191ed2415bb3edecc3d2371
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-10-19 18:53:24 -0600

    :recycle: Reuse div styles

--- src/pages/SeasonDetailsPage.tsx
+++ src/pages/SeasonDetailsPage.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import DivStyled from '../components/Layout/DivStyled';
 import PageLayout from '../components/Layout/PageLayout';
 import SeasonDetails from '../components/Season/SeasonDetails';
 
 export default function SeasonDetailsPage() {
   return (
     <PageLayout>
-      <div className='lg:w-9/12 md:w-11/12 sm:w-full bg-white md:p-6 p-2 mx-auto'>
+      <DivStyled>
         <SeasonDetails />
-      </div>
+      </DivStyled>
     </PageLayout>
   );
 }
